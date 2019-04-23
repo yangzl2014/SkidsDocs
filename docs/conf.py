19545ff6b8c142491068b4424b8da66b3d8fffbe
yangzl2014 <yangshenghello66@163.com>
2019-04-23 14:28:40 +0800

    删除templates

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['template']
+templates_path = ['templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
