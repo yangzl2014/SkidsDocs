4c4956a1dc3492d7eea800374e928da605cb6b76
yangzl2014 <yangshenghello66@163.com>
2019-04-23 14:40:32 +0800

    删除template调用 ，html_additional_pages = {}

--- docs/conf.py
+++ docs/conf.py
@@ -209,7 +209,8 @@ html_last_updated_fmt = '%d %b %Y'
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
-html_additional_pages = {"index": "topindex.html"}
+html_additional_pages = {}
+#html_additional_pages = {"index": "topindex.html"}
 
 # If false, no module index is generated.
 #html_domain_indices = True
